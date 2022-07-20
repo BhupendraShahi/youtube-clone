29a8dd18351085c01beb8cf37e232942407c7436
Bhupendra <shahi23.bhupendra11@gmail.com>
2022-07-20 21:43:07 +0530

    work on JWT and authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { response } from "express";
 import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     try {
@@ -19,8 +20,19 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
     try {
-        const user = await User.find({ name: req.body.name });
+        const user = await User.findOne({ name: req.body.name });
         if (!user) return next(createError(404, "User not found!"));
+
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isCorrect) return next(createError(400, "Wrong credentials!"));
+
+        const token = jwt.sign({ id: user._id }, "process.env.JWT");
+        const { password, ...others } = user._doc;
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(others);
     } catch (error) {
         next(err);
     }
