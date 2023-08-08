22003613d5b93d0e9a039c274b1d1c9f887f6119
Bhupendra <shahi23.bhupendra11@gmail.com>
2023-08-08 12:15:26 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -6,42 +6,55 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
 const connect = () => {
-    mongoose
-        .connect(process.env.MONGO)
-        .then(() => {
-            console.log("Connected to DB");
-        })
-        .catch((err) => {
-            throw err;
-        });
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET, POST, PUT, DELETE',
+  optionsSuccessStatus: 200, 
+  header: ['Cross-Origin-Opener-Policy', 'same-origin']   
+};
+
+app.use(cors(corsOptions));
+
+
 //middlewares
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
 
+
 //error handler
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong!";
-    return res.status(status).json({
-        success: false,
-        status,
-        message,
-    });
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
 
 app.listen(8800, () => {
-    connect();
-    console.log("Connected to Server");
+  connect();
+  console.log("Connected to Server");
 });
