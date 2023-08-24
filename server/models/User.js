006a3032d0573fd31b3c6edd00cf8af2ca251e02
Bhupendra Singh Shahi <shahi23.bhupendra11@gmail.com>
2023-08-24 11:49:44 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -33,4 +33,4 @@ const UserSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-export default mongoose.model("User", UserSchema);
+export default mongoose.model("youtubeusers", UserSchema);
